d5bb54cc8eafbddf98c940fa50c110cd35ff038c
Christian Nicolai <christian.nicolai@camunda.com>
2019-12-13 11:19:14 +0100

    Fiat lux

---
+++ README.md
@@ -0,0 +1,91 @@
+# kube-stale-resources
+
+This is a utility to detect stale resources in [Kubernetes](https://kubernetes.io/) clusters between resources from YAML manifests supplied via local file or stdin (target state) and a Kubernetes cluster (live state).
+All resources that exist in the live state but not in the target state are considered *stale* as they deviate from the intended state of the Kubernetes cluster (closed world assumption). It is intended as a complement to [kubectl diff](https://kubernetes.io/blog/2019/01/14/apiserver-dry-run-and-kubectl-diff/).
+
+Using a blacklist you can ignore resources from the live state from the comparison so they are not considered *stale* even though they do not exist in the given target state. This is useful when those resources are created by Kubernetes itself (e.g. the `kubernetes` service in the default namespace), managed by the Kubernetes cluster provider or another tool outside the scope.
+
+A use case for `kube-stale-resources` is using it alongside `kubectl diff` on locally present YAML manifests so `kubectl diff` can detect newly created Kubernetes resources and changes to those resources and `kube-stale-resources` can alert the user on stale resources that should be deleted from the cluster.
+
+Limitations:
+
+- currently only works on namespaced resources
+- currently requires explicit `metadata.namespace` field even for resources in default namespace
+- requires unauthenticated HTTP(S) access to Kubernetes apiserver, e.g. via `kubectl proxy`
+- only accounts for `apiVersion` deprecations up until Kubernetes 1.16
+
+
+## Usage
+
+You need Python 3.8 or higher.
+
+Assuming you have a properly setup `.kube/config` (e.g. using [minikube](https://github.com/kubernetes/minikube) after a successful `minikube start`) running:
+
+```bash
+kubectl proxy &
+
+# ignore minikube resources
+cat <<EOF > blacklist.txt
+^kubernetes-dashboard:.*$
+^default:events.k8s.io/v1beta1:Event:minikube..*$
+EOF
+
+# orderly created resource using YAML manifest in e.g. git
+cat <<EOF > version-controlled-resources.yml
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: foo
+  namespace: default
+spec:
+  type: ClusterIP
+  ports:
+    - port: 80
+      name: http
+      targetPort: 80
+  selector:
+    app: foo
+EOF
+kubectl apply -f version-controlled-resources.yml
+
+# on-the-fly created resource using imperative command
+kubectl create service clusterip bar --tcp='8080:8080'
+
+cat version-controlled-resources.yml | python kube-stale-resources.py -f - --blacklist blacklist.txt
+```
+
+This should yield a similar output to as we ignored what minikube sets up by default (e.g. the whole `kubernetes-dashboard` namespace):
+
+```
+Reading blacklist file blacklist.txt...
+Retrieving target state...
+Retrieving live state from http://localhost:8001...
+Live dynamic configmaps that are not in target (stale):
+.. 0 entries
+
+Live resources w/o dynamic configmaps that are not in target (stale):
+  default:v1:Service:bar
+.. 1 entries
+```
+
+Run `python kube-stale-resources.py -h` for full list of options.
+
+
+## Blacklisting
+
+Example blacklist file for a cluster on [GKE](https://cloud.google.com/kubernetes-engine/) that also uses [cert-manager](https://github.com/jetstack/cert-manager):
+
+```
+^.*:v1:ResourceQuota:gke-resource-quotas$
+^default:v1:LimitRange:limits$
+
+^.*:certmanager.k8s.io/v1alpha1:Order:.*$
+```
+
+In general a blacklist file contains one regular expression per line that are matched against a string of format `<namespace-name>:<apiVersion>:<kind>:<resource-name>` for each resource.
+
+
+## License
+
+kube-stale-resources is licensed under the Apache License, Version 2.0. See [LICENSE](LICENSE) for more information
---
+++ kube-stale-resources.py
@@ -0,0 +1,156 @@
+#!/usr/bin/env python
+
+import argparse
+import re
+import sys
+
+import requests
+import yaml
+
+from typing import (List, Any, Dict, Tuple, IO)
+K8sResourceIdentifier = Tuple[str, str, str, str]
+
+HEADERS = {"Content-Type": "application/json"}
+
+BLACKLIST_REGEXS = [
+  # Kubernetes inherent blacklist (should apply to every k8s cluster out there)
+  r'^.*:apps/v1:ControllerRevision:.*$',
+  r'^.*:apps/v1:ReplicaSet:.*$',
+  r'^.*:batch/v1:Job:.*-\d{10,}$',  # jobs created by cron jobs with unix timestamp suffix
+  r'^.*:metrics.k8s.io/v1beta1:PodMetrics:.*$',
+  r'^.*:v1:Endpoints:.*$',
+  r'^.*:v1:Event:.*$',
+  r'^.*:v1:Pod:.*$',
+  r'^.*:v1:Secret:.*-token-\S{5}$',  # secrets with token for service accounts
+  r'^.*:v1:ServiceAccount:default$',
+  r'^default:v1:Service:kubernetes$',
+  r'^kube-node-lease:.*$',
+  r'^kube-public:.*$',
+  r'^kube-system:.*$',
+
+  # GKE specific parts (should apply to every GKE-managed k8s cluster)
+  # '^.*:v1:ResourceQuota:gke-resource-quotas$',
+  # '^default:v1:LimitRange:limits$,
+]
+
+
+def get_live_namespaced_resources(url: str) -> List[K8sResourceIdentifier]:
+    result = []
+
+    # merges https://kubernetes.io/docs/reference/using-api/#api-groups
+
+    # legacy API group
+    apiVersions = requests.get(url + '/api', headers=HEADERS).json()['versions']
+    for apiVersion in apiVersions:
+        apiResources = requests.get(url + '/api/' + apiVersion, headers=HEADERS).json()['resources']
+        for apiResource in apiResources:
+            if not ('list' in apiResource['verbs'] and apiResource['namespaced']):
+                continue
+
+            items = requests.get(url + '/api/' + apiVersion + '/' + apiResource['name'],
+                                 headers=HEADERS).json()['items']
+            for item in items:
+                result.append(
+                  (item['metadata']['namespace'], apiVersion, apiResource['kind'], item['metadata']['name']))
+
+    # named API groups
+    apiGroups = requests.get(url + '/apis', headers=HEADERS).json()['groups']
+    for apiGroup in apiGroups:
+
+        apiResources = requests.get(url + '/apis/' + apiGroup['preferredVersion']['groupVersion'],
+                                    headers=HEADERS).json()['resources']
+        for apiResource in apiResources:
+            if not ('list' in apiResource['verbs'] and apiResource['namespaced']):
+                continue
+
+            if apiGroup['preferredVersion']['groupVersion'] == 'extensions/v1beta1' and apiResource['kind'] != 'Ingress':
+                # everything else in extensions/v1beta1 should be migrated to the preferred version
+                # except ingresses, see https://kubernetes.io/blog/2019/07/18/api-deprecations-in-1-16/
+                continue
+
+            items = requests.get(url + '/apis/' + apiGroup['preferredVersion']['groupVersion'] + '/' +
+                                 apiResource['name'],
+                                 headers=HEADERS).json()['items']
+            for item in items:
+                result.append((item['metadata']['namespace'], apiGroup['preferredVersion']['groupVersion'],
+                               apiResource['kind'], item['metadata']['name']))
+
+    return result
+
+
+def get_target_namespaced_resources(stream: IO) -> List[K8sResourceIdentifier]:
+    result = []
+
+    target_documents = list(yaml.load_all(stream, Loader=yaml.SafeLoader))
+    for document in target_documents:
+        if not document:
+            continue
+        if not 'namespace' in document['metadata']:
+            continue
+
+        result.append(
+          (document['metadata']['namespace'], document['apiVersion'], document['kind'], document['metadata']['name']))
+
+    return result
+
+
+def get_compact_resource_identifiers(tuples: List[K8sResourceIdentifier]) -> List[str]:
+    return [namespace + ':' + apiVersion + ':' + kind + ':' + name for namespace, apiVersion, kind, name in tuples]
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Utility to detect k8s configuration drift.')
+
+    parser.add_argument('-f',
+                        dest='target_manifests_file',
+                        required=True,
+                        help='File path (or - for stdin) to read Kubernetes manifests from for target state.')
+    parser.add_argument('--url',
+                        dest='k8s_apiserver_url',
+                        default='http://localhost:8001',
+                        help='URL of Kubernetes apiserver to retrieve live state.')
+    parser.add_argument('--blacklist',
+                        dest='blacklist_file',
+                        help='File path to read blacklist regex entries from that will be used to filter live state.')
+
+    args = parser.parse_args()
+
+    blacklist_regexs = []
+    blacklist_regexs += BLACKLIST_REGEXS
+
+    if args.blacklist_file:
+        print(f'Reading blacklist file {args.blacklist_file}...')
+        with open(args.blacklist_file, 'r') as f:
+            blacklist_regexs += filter(lambda x: not re.match(r'^\s*$', x), f.read().split('\n'))
+
+    print('Retrieving target state...')
+    if args.target_manifests_file == '-':
+        target_tuples = get_target_namespaced_resources(sys.stdin)
+    else:
+        with open(args.target_manifests_file, 'r') as f:
+            target_tuples = get_target_namespaced_resources(f)
+
+    print(f'Retrieving live state from {args.k8s_apiserver_url}...')
+    raw_live_strings = get_compact_resource_identifiers(get_live_namespaced_resources(args.k8s_apiserver_url))
+
+    live_strings = list(filter(lambda s: not re.match('|'.join(blacklist_regexs), s), raw_live_strings))
+
+    starget = set(get_compact_resource_identifiers(target_tuples))
+    slive = set(live_strings)
+
+    print('Live dynamic configmaps that are not in target (stale):')
+    counter = 0
+    for x in sorted(list(slive - starget)):
+        if re.match('^.*:v1:ConfigMap:.*-[a-z0-9]{10}', x):
+            counter += 1
+            print('  ' + x)
+    print("..", counter, "entries")
+
+    print()
+    print('Live resources w/o dynamic configmaps that are not in target (stale):')
+    counter = 0
+    for x in sorted(list(slive - starget)):
+        if not re.match('^.*:v1:ConfigMap:.*-[a-z0-9]{10}', x):
+            counter += 1
+            print('  ' + x)
+    print("..", counter, "entries")
